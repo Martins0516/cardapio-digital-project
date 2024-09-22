2394b521fe9c9332f6066aea18c2b6adebbe6734
Gabriel Martins <gabrielmartins0506@gmail.com>
2024-09-22 00:34:33 -0300

    Corrigindo Navbar

--- script.js
+++
@@ -1,6 +0,0 @@
-const navLinks = document.querySelector('.nav-links')
-function onToggleMenu(e) {
-    e.name = e.name === 'menu-outline' ? 'close' : 'menu-outline'
-    navLinks.classList.toggle('top-[9%]')
-}
-// TÁ DANDO ERRO NA HORA DE CLICAR E APARECER O MENU - VERSÃO MOBILE
